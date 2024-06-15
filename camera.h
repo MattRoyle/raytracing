5ff98f5e15a3c2a6cf3b14c3ad2caf1f83cd90ec
psymr5 <psymr5@nottingham.ac.uk>
2024-06-15 20:28:40 +0100

    added camera class

---
+++ camera.h
@@ -0,0 +1,71 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "headers.h"
+
+#include "hittable.h"
+
+class camera {
+  public:
+    double aspect_ratio = 16.0 / 9.0;//aspect ration is ideal ratio
+    int image_width = 400;
+
+    void render(const hittable& world) {
+        std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";//formatting the .ppm file
+        for (int j = 0; j < image_height; j++) {
+            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;//writes to the console
+            for (int i = 0; i < image_width; i++) {
+                point3 pixel_center = pixel_origin + (i*pixel_delta_u)+(j*pixel_delta_v);
+                vec3 ray_dir = pixel_center - camera_center;
+                ray ray(camera_center,ray_dir);
+
+                color pixel_color = ray_color(ray, world);// creates a colour using the x and y positions
+                write_color(std::cout, pixel_color);//writes to the out
+            }
+        }
+        std::clog << "\rDone.                 \n";
+    }
+
+  private:
+    int    image_height;   // Rendered image height
+    point3 camera_center;         // Camera center
+    point3 pixel_origin;    // Location of pixel 0, 0
+    vec3   pixel_delta_u;  //horizontal pixel offset
+    vec3   pixel_delta_v; // vertical pixel offset
+
+    void initialize() {
+       
+        image_height = int(image_width / aspect_ratio);
+        image_height = (image_height < 1) ? 1 : image_height; // calculate the image height, and ensure that it's at least 1.
+
+        auto focal_length = 1.0; //inital value of orthagonal dist from viewport
+        camera_center = point3(0, 0, 0);
+        // Viewport widths less than one are ok since they are real valued.
+        auto viewport_height = 2.0;
+        auto viewport_width = viewport_height * (double(image_width)/image_height);
+        //image height and width are int versions so only aproximations of the values calculated by the aspect ratio 
+
+        auto viewport_u = vec3(viewport_width, 0, 0);//horizontal vector of viewport
+        auto viewport_v = vec3(0, -viewport_height, 0);//vertical vector of viewport
+
+        pixel_delta_u = viewport_u / image_width;
+        pixel_delta_v = viewport_v / image_height;
+
+        // Calculate the location of the upper left pixel.
+        auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        pixel_origin = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    }
+
+    color ray_color(const ray& r, const hittable& world) {
+        hit_record record;
+        if (world.hit(r, interval(0, INF), record)) {
+            return 0.5 * (record.normal + color(1,1,1));
+        }
+
+        vec3 unit_dir = unit_vector(r.direction());
+        auto at = 0.5*(unit_dir.y()+1.0);
+        return lerp(color(1.0,1.0,1.0), color(0.5,0.7,1.0), at);
+    }
+};
+
+#endif
